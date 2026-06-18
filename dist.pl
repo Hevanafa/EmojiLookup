051239399c92d4038e8611d2f229ddcb1fbad614
Hevanafa <dhevansafa4@gmail.com>
2026-06-18 16:39:13 +0700

    added build date string detail

--- dist.pl
+++ dist.pl
@@ -4,21 +4,29 @@ use 5.38.2;
 
 use File::Copy qw(copy);
 use File::Copy::Recursive qw(dircopy);
+use File::Spec::Functions qw(catfile);
+
+use Time::Piece;
 
 my @files = (
   "EmojiLookup.exe",
+
   "LICENSE",
-  "LICENSE-Unicode.txt"
+  "LICENSE-Unicode.txt",
+  "readme.md"
 );
 
-mkdir "dist" unless -d "dist";
+my $date_str = localtime->strftime("%d.%m.%Y");
+my $build_dir = catfile("builds", $date_str."_test");
+
+mkdir $build_dir unless -d $build_dir;
 
 for my $file (@files) {
-  copy $file, "dist"
+  copy $file, $build_dir
 }
 
-dircopy "data", "dist/data";
+dircopy "data", "$build_dir/data";
 say "Copied Unicode emoji data";
 
-rename "dist/LICENSE", "dist/LICENSE.TXT"
+rename "dist/LICENSE", "dist/LICENSE.TXT";
 say "Renamed LICENSE.txt"
